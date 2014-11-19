c8c647e104d1e15607c41e78d08b6c351a15faad
doxtop <andrii.zadorozhnii@gmail.com>
2014-11-19 16:40:44 +0200

    remove colors

--- src/main/scala/plugin.scala
+++ src/main/scala/plugin.scala
@@ -1,70 +1,19 @@
 package synrc.ci
 
+import java.io.File
+import java.lang.{Runtime => JRuntime}
 import java.util.concurrent.atomic.AtomicReference
 import java.util.jar.Manifest
 
 import sbt.Keys._
 import sbt._
-import java.lang.{Runtime => JRuntime}
-import java.io.File
-import scala.Console._
 
+import scala.Console._
 import scala.annotation.tailrec
-import scala.collection.immutable.Queue
 
 object CiPlugin extends AutoPlugin {
 
-  object Utilities {
-    def colorLogger(state: State): Logger = colorLogger(state.log)
-
-    def colorLogger(logger: Logger): Logger = new Logger {
-      def trace(t: => Throwable) { logger.trace(t) }
-      def success(message: => String) { success(message) }
-      def log(level: Level.Value, message: => String): Unit =
-        logger.log(level, colorize(logger.ansiCodesSupported, message))
-    }
-
-    val simpleColors =
-      Seq(
-        "RED" -> RED,
-        "GREEN" -> GREEN,
-        "YELLOW" -> YELLOW,
-        "BLUE" -> BLUE,
-        "MAGENTA" -> MAGENTA,
-        "CYAN" -> CYAN,
-        "WHITE" -> WHITE
-      )
-    val rgbColors = (0 to 255) map rgb
-    val ansiTagMapping: Seq[(String, String)] =
-      (
-        Seq(
-          "BOLD" -> BOLD,
-          "RESET" -> RESET
-        ) ++
-          simpleColors ++
-          simpleColors.map(reversed) ++
-          simpleColors.map(underlined) ++
-          rgbColors
-        ).map(delimited("[", "]"))
-
-    def reversed(color: (String, String)): (String, String) =
-      ("~"+color._1) -> (color._2+REVERSED)
-    def underlined(color: (String, String)): (String, String) =
-      ("_"+color._1) -> (color._2+UNDERLINED)
-    def delimited(before: String, after: String)(mapping: (String, String)): (String, String) =
-      (before+mapping._1+after, mapping._2)
-    def rgb(idx: Int): (String, String) = ("RGB"+idx, "\033[38;5;"+idx+"m")
-
-    def replaceAll(message: String, replacer: String => String) =
-      ansiTagMapping.foldLeft(message)((msg, tag) => msg.replaceAll(java.util.regex.Pattern.quote(tag._1), replacer(tag._2)))
-
-    def colorize(ansiCodesSupported: Boolean, message: String): String =
-      replaceAll(message, if (ansiCodesSupported) identity else _ => "")
-  }
-
-  import Utilities._
-
-  class SysoutLogger(appName: String, color: String, ansiCodesSupported: Boolean = false) extends Logger {
+  class SysoutLogger(appName: String) extends Logger {
 
     def trace(t: => Throwable) {
       t.printStackTrace()
@@ -72,7 +21,7 @@ object CiPlugin extends AutoPlugin {
     }
 
     def success(message: => String) {
-      println(Utilities.colorize(ansiCodesSupported, "%s%s[RESET] success: " format (color, appName)) + message)
+      println("%s success: " + appName + message)
     }
 
     def log(level: Level.Value, message: => String):Unit= println((level match {
@@ -104,38 +53,19 @@ object CiPlugin extends AutoPlugin {
     def get(): RevolverState = state.get()
   }
 
-  case class RevolverState(processes: Map[ProjectRef, AppProcess], colorPool: Queue[String]) {
+  case class RevolverState(processes: Map[ProjectRef, AppProcess]) {
     def addProcess(project: ProjectRef, process: AppProcess): RevolverState = copy(processes = processes + (project -> process))
-    private[this] def removeProcess(project: ProjectRef): RevolverState = copy(processes = processes - project)
-    def removeProcessAndColor(project: ProjectRef): RevolverState =
-      getProcess(project) match {
-        case Some(process) => removeProcess(project).offerColor(process.consoleColor)
-        case None => this
-      }
-
+    def removeProcess(project: ProjectRef): RevolverState = copy(processes = processes - project)
     def exists(project: ProjectRef): Boolean = processes.contains(project)
     def runningProjects: Seq[ProjectRef] = processes.keys.toSeq
     def getProcess(project: ProjectRef): Option[AppProcess] = processes.get(project)
-
-    def takeColor: (RevolverState, String) =
-      if (colorPool.nonEmpty) {
-        val (color, nextPool) = colorPool.dequeue
-        (copy(colorPool = nextPool), color)
-      } else (this, "")
-
-    def offerColor(color: String): RevolverState =
-      if (color.nonEmpty) copy(colorPool = colorPool.enqueue(color))
-      else this
   }
 
   object RevolverState {
-    def initial = RevolverState(Map.empty, Queue.empty)
+    def initial = RevolverState(Map.empty)
   }
 
-  /**
-   * A token which we put into the SBT state to hold the Process of an application running in the background.
-   */
-  case class AppProcess(projectRef: ProjectRef, consoleColor: String, log: Logger)(process: Process) {
+  case class AppProcess(projectRef: ProjectRef, log: Logger)(process: Process) {
     val shutdownHook = createShutdownHook("... killing ...")
 
     def createShutdownHook(msg: => String) =
@@ -179,8 +109,6 @@ object CiPlugin extends AutoPlugin {
   }
 
   object Actions {
-    import Utilities._
-
     def restartApp(streams: TaskStreams, logTag: String, project: ProjectRef, option: ForkOptions, mainClass: Option[String],
                    cp: Classpath, args: Seq[String], startConfig: ExtraCmdLineOptions): AppProcess = {
       stopAppWithStreams(streams, project)
@@ -193,30 +121,27 @@ object CiPlugin extends AutoPlugin {
 
       // fail early
       val theMainClass = mainClass.get
-      val color = updateStateAndGet(_.takeColor)
-      val logger = new SysoutLogger(logTag, color, streams.log.ansiCodesSupported)
-      colorLogger(streams.log).info("[YELLOW]Starting application %s in the background ..." format formatAppName(project.project, color))
+      val logger = new SysoutLogger(logTag)
+      streams.log.info(s"Starting application ${project.project} in the background ...")
 
-      val appProcess=
-        AppProcess(project, color, logger) {
+      val appProcess= AppProcess(project, logger) {
           forkRun(options, theMainClass, cp.map(_.data), args ++ startConfig.startArgs, logger, startConfig.jvmArgs)
-        }
+      }
       registerAppProcess(project, appProcess)
       appProcess
     }
 
-    def stopAppWithStreams(streams: TaskStreams, project: ProjectRef) = stopApp(colorLogger(streams.log), project)
+    def stopAppWithStreams(streams: TaskStreams, project: ProjectRef) = stopApp(streams.log, project)
 
     def stopApp(log: Logger, project: ProjectRef): Unit = {
       revolverState.getProcess(project) match {
         case Some(appProcess) =>
           if (appProcess.isRunning) {
-            log.info("Stopping application %s (by killing the forked JVM) ..." format formatApp(appProcess))
-
+            log.info("Stopping application %s (by killing the forked JVM) ..." format appProcess.projectName)
             appProcess.stop()
           }
         case None =>
-          log.info("Application %s not yet started" format formatAppName(project.project, ""))
+          log.info("Application %s not yet started" format project.project)
       }
       unregisterAppProcess(project)
     }
@@ -225,12 +150,12 @@ object CiPlugin extends AutoPlugin {
       revolverState.runningProjects.foreach(stopApp(log, _))
 
     def showStatus(streams: TaskStreams, project: ProjectRef): Unit =
-      colorLogger(streams.log).info {
+      streams.log.info {
         revolverState.getProcess(project).find(_.isRunning) match {
           case Some(appProcess) =>
-            "[GREEN]Application %s is currently running" format formatApp(appProcess, color = "[GREEN]")
+            s"Application ${formatApp(appProcess)} is currently running"
           case None =>
-            "[YELLOW]Application %s is currently NOT running" format formatAppName(project.project, "[BOLD]")
+            s"Application ${project.project} is currently NOT running"
         }
       }
 
@@ -249,12 +174,12 @@ object CiPlugin extends AutoPlugin {
         state.addProcess(project, process)
       }
 
-    def unregisterAppProcess(project: ProjectRef) = updateState(_.removeProcessAndColor(project))
+    def unregisterAppProcess(project: ProjectRef) = updateState(_.removeProcess(project))
 
     case class ExtraCmdLineOptions(jvmArgs: Seq[String], startArgs: Seq[String])
 
-    import sbt.complete.Parsers._
     import sbt.complete.Parser._
+    import sbt.complete.Parsers._
     val spaceDelimitedWithoutDashes =
       (token(Space) ~> (token(NotSpace, "<args>") - "---")).* <~ SpaceClass.*
     /*
@@ -268,10 +193,7 @@ object CiPlugin extends AutoPlugin {
       }
     }
 
-    def formatApp(process: AppProcess, color: String = "[YELLOW]"): String =
-      formatAppName(process.projectName, process.consoleColor, color)
-    def formatAppName(projectName: String, projectColor: String, color: String = "[YELLOW]"): String =
-      "[RESET]%s%s[RESET]%s" format (projectColor, projectName, color)
+    def formatApp(process: AppProcess): String = process.projectName
   }
 
   object autoImport{
@@ -292,8 +214,6 @@ object CiPlugin extends AutoPlugin {
 
     val reForkOptions = TaskKey[ForkOptions]("re-fork-options", "The options needed for the start task for forking")
 
-    val reColors = SettingKey[Seq[String]]("re-colors", "Colors used for tagging output from different processes")
-
     // we need this strange setup to make sure we can define a project specific default
     // we cannot put the project specific one `in Global` as it isn't Global
     val reLogTagUnscoped = SettingKey[String]("re-log-tag", "The tag used in front of log messages for this project")
@@ -313,8 +233,8 @@ object CiPlugin extends AutoPlugin {
   override def requires = plugins.JvmPlugin
   override def trigger = allRequirements
 
-  import Actions._
-  import autoImport._
+  import synrc.ci.CiPlugin.Actions._
+  import synrc.ci.CiPlugin.autoImport._
 
   lazy val warSettings: Seq[Setting[_]] =
     Defaults.packageTaskSettings(packageWar, prepareWebappTask) ++
@@ -437,21 +357,15 @@ object CiPlugin extends AutoPlugin {
 
   override val globalSettings:Seq[Def.Setting[_]] = Seq(
     onLoad in Global := (onLoad in Global).value andThen { state =>
-      val colorTags = (reColors in reStart).value.map(_.toUpperCase formatted "[%s]")
-      GlobalState.update(_.copy(colorPool = collection.immutable.Queue(colorTags: _*)))
+      GlobalState.update(_.copy())
       state
     },
     onUnload in Global := (onUnload in Global).value andThen {state =>
-      stopApps(colorLogger(state))
+      stopApps(state.log)
       state
     },
-    reColors in Global in reStart := basicColors,
     reStartArgs in Global := Seq.empty
   )
-
-  def noColors: Seq[String] = Nil
-  def basicColors = Seq("BLUE", "MAGENTA", "CYAN", "YELLOW", "GREEN")
-  def basicColorsAndUnderlined = basicColors ++ basicColors.map("_"+_)
 
 
   private def portArg(port: Int): Seq[String] =
@@ -463,10 +377,9 @@ object CiPlugin extends AutoPlugin {
   def tomcat(libs: Seq[ModuleID] = Seq(tomcatRunner),
              main: String        = "webapp.runner.launch.Main",
              port: Int           = -1,
-             args: Seq[String]   = Nil,
-             options: ForkOptions = new ForkOptions): Seq[Setting[_]] = {
+             args: Seq[String]   = Nil): Seq[Setting[_]] = {
     val runnerArgs = Seq(main) ++ portArg(port) ++ args
-    runnerContainer(libs, runnerArgs, options) ++ warSettings ++ webappSettings
+    runnerContainer(libs, runnerArgs) ++ warSettings ++ webappSettings
   }
 
   def forkRun(config: ForkOptions, mainClass: String, classpath: Seq[File], options: Seq[String], log: Logger, extraJvmArgs: Seq[String]): Process = {
@@ -480,8 +393,6 @@ object CiPlugin extends AutoPlugin {
     Fork.scala.fork(newOptions, scalaOptions)
   }
 
-
-
   private def shutdown(l: Logger, atomicRef: AtomicReference[Option[Process]]): Unit = {
     val oldProcess = atomicRef.getAndSet(None)
     oldProcess.foreach(stopProcess(l))
@@ -493,9 +404,7 @@ object CiPlugin extends AutoPlugin {
     p.exitValue
   }
 
-  private def startup(
-                       l: Logger, libs: Seq[File], args: Seq[String], forkOptions: ForkOptions
-                       ): Process = {
+  private def startup(l: Logger, libs: Seq[File], args: Seq[String], forkOptions: ForkOptions): Process = {
     l.info("starting server ...")
     val cp = libs mkString File.pathSeparator
     Fork.java.fork(forkOptions, Seq("-cp", cp) ++ args)
@@ -541,9 +450,8 @@ object CiPlugin extends AutoPlugin {
     }
   }
 
-  def containerSettings(
-                         launchCmdTask: Def.Initialize[Task[Seq[String]]],
-                         forkOptions: ForkOptions
+  def containerSettings(launchCmdTask: Def.Initialize[Task[Seq[String]]],
+                         forkOptions: ForkOptions = new ForkOptions
                          ): Seq[Setting[_]] = {
     val atomicRef: AtomicReference[Option[Process]] = new AtomicReference(None)
 
@@ -558,10 +466,8 @@ object CiPlugin extends AutoPlugin {
     } ++ Seq(ivyConfigurations += container)
   }
 
-  def runnerContainer(
-                       libs: Seq[ModuleID], args: Seq[String], forkOptions: ForkOptions = new ForkOptions
-                       ): Seq[Setting[_]] =
+  def runnerContainer(libs: Seq[ModuleID], args: Seq[String]): Seq[Setting[_]] =
     Seq(libraryDependencies ++= libs) ++
-      containerSettings((webappDest in webapp) map { d => args :+ d.getPath }, forkOptions)
+      containerSettings((webappDest in webapp) map { d => args :+ d.getPath })
 
 }
