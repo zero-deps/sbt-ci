3b5dba30379cfe28f59fcd5787e21a1bdd2073d4
doxtop <andrii.zadorozhnii@gmail.com>
2014-12-11 13:24:12 +0200

    js working folder + react jsx

---
+++ src/main/resources/js/ci.js
@@ -0,0 +1,44 @@
+var React = require('react');
+var View = require('./view.jsx');
+
+var data = [
+  {name: "task", text: "This is one"},
+  {name: "another task", text: "This is *another*"}
+];
+
+var sbt_task = React.createClass({displayName:'task',
+  render: function(){
+    return React.createElement('li', null, this.props.name);
+  }
+});
+
+var sbt_tasks = React.createClass({displayName:'tasks',
+  render: function(){
+    var taskNodes = this.props.data.map(function(task){
+      return (
+        React.createElement(sbt_task,{name:task.name})
+      );
+    });
+    return (
+      React.createElement('div', {className:"sbt tasks"}, taskNodes)
+    );
+  }
+});
+
+var ws = {};
+ws.get = function(url,callback){
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+      if (req.readyState == 4 && req.status == 200) {callback(req.response);}
+  };
+  req.open('GET', url, true);
+  req.send();
+};
+
+React.render(React.createElement(sbt_tasks, {data:data}), document.getElementById('bdy'));
+
+/*Reveal.initialize({history: true,center: true,embedded: true,
+  dependencies: [
+    {src: 'assets/js/marked.js', condition: function() { return !!document.querySelector('[data-markdown]');}},
+    {src: 'assets/js/markdown.js', condition: function() { return !!document.querySelector('[data-markdown]');}} ]});*/
+//React.render(<View/>, document.body);
---
+++ src/main/resources/js/view.jsx
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var View = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Example View 3
+      </div>
+    );
+  }
+});
+
+module.exports = View;
